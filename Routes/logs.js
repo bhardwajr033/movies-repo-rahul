ac4fdef2c59a7407a247456fe9bbc90c456e9e82
rahulbhardwaj <rahul.bhardwaj.23.3@mountblue.tech>
2023-05-06 17:54:35 +0530

    modified log route to use async await

--- Routes/logs.js
+++ Routes/logs.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const logs_router = express.Router();
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 
-logs_router.get("/", (req, res) => {
+logs_router.get("/", async (req, res) => {
   try {
     const logFilePath = path.join(__dirname, "../Logs/access.log");
-    const logs = fs.readFileSync(logFilePath, "utf-8");
+    const logs = await fs.readFile(logFilePath, "utf-8");
     res.send(logs);
   } catch (err) {
     console.log(err);
