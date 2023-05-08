6e8c4352efffd72a213d6deda23d6b29970cf75d
rahulbhardwaj <rahul.bhardwaj.23.3@mountblue.tech>
2023-05-08 18:19:32 +0530

    movies all 5 api completed

--- Routes/movie_routes.js
+++ Routes/movie_routes.js
@@ -1,15 +1,19 @@
 const express = require("express");
 
 const moviesRouter = express.Router();
-const dbConfig = require("../database/DBconfig");
+const prisma = require("../database/DBconfig");
 
 moviesRouter.get("/", async (req, res) => {
   try {
-    const movies = await dbConfig.query("SELECT * FROM movies;");
-    if (movies.rows.length === 0) {
+    const movies = await prisma.Movies.findMany({
+      include: {
+        director: true,
+      },
+    });
+    if (!movies) {
       res.send(["No movies found"]);
     } else {
-      res.json(movies.rows);
+      res.json(movies);
     }
   } catch (err) {
     console.log(err);
@@ -20,51 +24,55 @@ moviesRouter.get("/", async (req, res) => {
 moviesRouter.post("/", async (req, res) => {
   try {
     const movie = req.body;
-    await dbConfig.query(
-      "INSERT INTO directors (director_name) VALUES ($1);",
-      [movie.director_name]
-    );
-    const dbRespose = await dbConfig.query(
-      "INSERT INTO movies (rank, title, description, runtime, genre, rating, metascore, votes, gross_earning_in_mil, director_name, actor, release_year , director_id) VALUES ($1, $2 , $3 , $4 , $5 , $6 , $7 , $8 , $9 , $10 , $11 , $12,(SELECT id FROM directors WHERE director_name = $13)) RETURNING *;",
-      [
-        movie.rank,
-        movie.title,
-        movie.description,
-        movie.runtime,
-        movie.genre,
-        movie.rating,
-        movie.metascore,
-        movie.votes,
-        movie.gross_earning_in_mil,
-        movie.director_name,
-        movie.actor,
-        movie.release_year,
-        movie.director_name
-      ]
-    );
-    res.json(dbRespose.rows);
+    const director = await prisma.Directors.upsert({
+      where: { name: movie.director },
+      update: {},
+      create: { name: movie.director },
+    });
+
+    const movieAdded = await prisma.Movies.create({
+      data: {
+        rank: movie.rank,
+        title: movie.title,
+        description: movie.description,
+        runtime: movie.runtime,
+        genre: movie.genre,
+        rating: movie.rating,
+        metascore: movie.metascore,
+        votes: movie.votes,
+        grossEarningsInMil: movie.grossEarningsInMil,
+        director: {
+          connect: { id: director.id },
+        },
+        releaseYear: movie.releaseYear,
+      },
+      include: {
+        director: true,
+      },
+    });
+
+    res.json(movieAdded);
   } catch (err) {
     console.log(err);
-    if (
-      err.message ===
-      'duplicate key value violates unique constraint "movies_rank_key"'
-    ) {
-      res.status(600).send({ Error: "Movie with that rank already exists" });
-    } else {
-      res.status(500).send({ Error: err.message });
-    }
+    res.status(500).send({ Error: err.message });
   }
 });
 
 moviesRouter.get("/:movieId", async (req, res) => {
   try {
-    const movie = await dbConfig.query(
-      `SELECT * FROM movies WHERE rank = ${parseInt(req.params.movieId,10)};`
-    );
-    if (movie.rows.length === 0) {
+    const movie = await prisma.Movies.findUnique({
+      where: {
+        rank: Number(req.params.movieId),
+      },
+      include: {
+        director: true,
+      },
+    });
+
+    if (!movie) {
       res.send([`No movies found with movieid of ${req.params.movieId}`]);
     } else {
-      res.json(movie.rows);
+      res.json(movie);
     }
   } catch (err) {
     console.log(err);
@@ -74,35 +82,40 @@ moviesRouter.get("/:movieId", async (req, res) => {
 
 moviesRouter.put("/:movieId", async (req, res) => {
   try {
-    const updatedDetails = req.body;
-    const dbResposeMovie = await dbConfig.query(
-      `SELECT * FROM movies WHERE rank = ${parseInt(req.params.movieId,10)};`
-    );
-    const {rows} = dbResposeMovie;
-    const movie = rows[0];
-    if (movie.length === 0) {
-      res.send([`No movies found with movieid of ${req.params.movieId}`]);
-      return;
-    }
-    const dbRespose = await dbConfig.query(
-      `UPDATE movies SET  title = $1, description =  $2, runtime = $3, genre = $4, rating = $5, metascore = $6, votes = $7, gross_earning_in_mil = $8, director_name = $9, actor = $10, release_year = $11  WHERE rank = ${parseInt(
-        req.params.movieId,10
-      )} RETURNING *;`,
-      [
-        updatedDetails.title || movie.title,
-        updatedDetails.description || movie.description,
-        updatedDetails.runtime || movie.runtime,
-        updatedDetails.genre || movie.genre,
-        updatedDetails.rating || movie.rating,
-        updatedDetails.metascore || movie.metascore,
-        updatedDetails.votes || movie.votes,
-        updatedDetails.gross_earning_in_mil || movie.gross_earning_in_mil,
-        updatedDetails.director_name || movie.director_name,
-        updatedDetails.actor || movie.actor,
-        updatedDetails.release_year || movie.release_year,
-      ]
-    );
-    res.json(dbRespose.rows);
+    const movie = req.body;
+
+    const directorDetail = await prisma.Movies.findUnique({
+      where: {
+        rank: Number(req.params.movieId),
+      },
+      include: {
+        director: true,
+      },
+    });
+    const directorName = directorDetail.director.name;
+
+    const director = await prisma.Directors.upsert({
+      where: { name: movie.director || directorName },
+      update: {},
+      create: { name: movie.director || directorName },
+    });
+
+    const movieUpdated = await prisma.Movies.update({
+      where: {
+        rank: Number(req.params.movieId),
+      },
+      data: {
+        ...movie,
+        director: {
+          connect: { id: director.id },
+        },
+      },
+      include: {
+        director: true,
+      },
+    });
+
+    res.json(movieUpdated);
   } catch (err) {
     console.log(err);
     res.status(500).send([{ Error: err.message }]);
@@ -111,15 +124,15 @@ moviesRouter.put("/:movieId", async (req, res) => {
 
 moviesRouter.delete("/:movieId", async (req, res) => {
   try {
-    const dbRespose = await dbConfig.query(
-      `DELETE FROM movies WHERE rank = ${parseInt(
-        req.params.movieId,10
-      )} RETURNING *;`
-    );
-    if (dbRespose.rows.length === 0) {
+    const movieDeleted = await prisma.Movies.delete({
+      where: {
+        rank: Number(req.params.movieId),
+      },
+    });
+    if (!movieDeleted) {
       res.send([`No movies found with movieid of ${req.params.movieId}`]);
     } else {
-      res.json(dbRespose.rows);
+      res.json(movieDeleted);
     }
   } catch (err) {
     console.log(err);
